58d227734cde4b28fa337dbe6b2eeb5662f1107a
zibin <270206244@qq.com>
2017-06-07 18:45:37 +0800

    no message

--- example/src/main/java/top/zibin/luban/example/MainActivity.java
+++ example/src/main/java/top/zibin/luban/example/MainActivity.java
@@ -23,129 +23,128 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
-import top.zibin.luban.Luban;
+import top.zibin.luban.LubanOld;
 import top.zibin.luban.OnCompressListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView fileSize;
-    private TextView imageSize;
-    private TextView thumbFileSize;
-    private TextView thumbImageSize;
-    private ImageView image;
+  private TextView fileSize;
+  private TextView imageSize;
+  private TextView thumbFileSize;
+  private TextView thumbImageSize;
+  private ImageView image;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        fileSize = (TextView) findViewById(R.id.file_size);
-        imageSize = (TextView) findViewById(R.id.image_size);
-        thumbFileSize = (TextView) findViewById(R.id.thumb_file_size);
-        thumbImageSize = (TextView) findViewById(R.id.thumb_image_size);
-        image = (ImageView) findViewById(R.id.image);
+    fileSize = (TextView) findViewById(R.id.file_size);
+    imageSize = (TextView) findViewById(R.id.image_size);
+    thumbFileSize = (TextView) findViewById(R.id.thumb_file_size);
+    thumbImageSize = (TextView) findViewById(R.id.thumb_image_size);
+    image = (ImageView) findViewById(R.id.image);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PhotoPicker.builder()
-                        .setPhotoCount(5)
-                        .setShowCamera(true)
-                        .setShowGif(true)
-                        .setPreviewEnabled(false)
-                        .start(MainActivity.this, PhotoPicker.REQUEST_CODE);
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        PhotoPicker.builder()
+            .setPhotoCount(5)
+            .setShowCamera(true)
+            .setShowGif(true)
+            .setPreviewEnabled(false)
+            .start(MainActivity.this, PhotoPicker.REQUEST_CODE);
 
-            }
+      }
+    });
+  }
+
+  /**
+   * 压缩单张图片 Listener 方式
+   */
+  private void compressWithLs(File file) {
+    LubanOld.get(this)
+        .load(file)
+        .putGear(LubanOld.THIRD_GEAR)
+        .setCompressListener(new OnCompressListener() {
+          @Override
+          public void onStart() {
+            Toast.makeText(MainActivity.this, "I'm start", Toast.LENGTH_SHORT).show();
+          }
+
+          @Override
+          public void onSuccess(File file) {
+            Log.i("path", file.getAbsolutePath());
+
+            Glide.with(MainActivity.this).load(file).into(image);
+
+            thumbFileSize.setText(file.length() / 1024 + "k");
+            thumbImageSize.setText(LubanOld.get(getApplicationContext()).getImageSize(file.getPath())[0] + " * " + LubanOld.get(getApplicationContext()).getImageSize(file.getPath())[1]);
+          }
+
+          @Override
+          public void onError(Throwable e) {
+
+          }
+        }).launch();
+  }
+
+  /**
+   * 压缩单张图片 RxJava 方式
+   */
+  private void compressWithRx(File file) {
+    LubanOld.get(this)
+        .load(file)
+        .putGear(LubanOld.THIRD_GEAR)
+        .asObservable()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnError(new Action1<Throwable>() {
+          @Override
+          public void call(Throwable throwable) {
+            throwable.printStackTrace();
+          }
+        })
+        .onErrorResumeNext(new Func1<Throwable, Observable<? extends File>>() {
+          @Override
+          public Observable<? extends File> call(Throwable throwable) {
+            return Observable.empty();
+          }
+        })
+        .subscribe(new Action1<File>() {
+          @Override
+          public void call(File file) {
+            Glide.with(MainActivity.this).load(file).into(image);
+
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri uri = Uri.fromFile(file);
+            intent.setData(uri);
+            MainActivity.this.sendBroadcast(intent);
+
+            thumbFileSize.setText(file.length() / 1024 + "k");
+            thumbImageSize.setText(LubanOld.get(getApplicationContext()).getImageSize(file.getPath())[0] + " * " + LubanOld.get(getApplicationContext()).getImageSize(file.getPath())[1]);
+          }
         });
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
+      if (data != null) {
+        ArrayList<String> photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+
+        File imgFile = new File(photos.get(0));
+        fileSize.setText(imgFile.length() / 1024 + "k");
+        imageSize.setText(LubanOld.get(this).getImageSize(imgFile.getPath())[0] + " * " + LubanOld.get(this).getImageSize(imgFile.getPath())[1]);
+
+        for (int i = 0; i < photos.size(); i++)
+          compressWithLs(new File(photos.get(i)));
+      }
     }
-
-    /**
-     * 压缩单张图片 Listener 方式
-     */
-    private void compressWithLs(File file) {
-        Luban.get(this)
-                .load(file)
-                .putGear(Luban.THIRD_GEAR)
-                .setFilename(System.currentTimeMillis() + "")
-                .setCompressListener(new OnCompressListener() {
-                    @Override
-                    public void onStart() {
-                        Toast.makeText(MainActivity.this, "I'm start", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onSuccess(File file) {
-                        Log.i("path", file.getAbsolutePath());
-
-                        Glide.with(MainActivity.this).load(file).into(image);
-
-                        thumbFileSize.setText(file.length() / 1024 + "k");
-                        thumbImageSize.setText(Luban.get(getApplicationContext()).getImageSize(file.getPath())[0] + " * " + Luban.get(getApplicationContext()).getImageSize(file.getPath())[1]);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-                }).launch();
-    }
-
-    /**
-     * 压缩单张图片 RxJava 方式
-     */
-    private void compressWithRx(File file) {
-        Luban.get(this)
-                .load(file)
-                .putGear(Luban.THIRD_GEAR)
-                .asObservable()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnError(new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                })
-                .onErrorResumeNext(new Func1<Throwable, Observable<? extends File>>() {
-                    @Override
-                    public Observable<? extends File> call(Throwable throwable) {
-                        return Observable.empty();
-                    }
-                })
-                .subscribe(new Action1<File>() {
-                    @Override
-                    public void call(File file) {
-                        Glide.with(MainActivity.this).load(file).into(image);
-
-                        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                        Uri uri = Uri.fromFile(file);
-                        intent.setData(uri);
-                        MainActivity.this.sendBroadcast(intent);
-
-                        thumbFileSize.setText(file.length() / 1024 + "k");
-                        thumbImageSize.setText(Luban.get(getApplicationContext()).getImageSize(file.getPath())[0] + " * " + Luban.get(getApplicationContext()).getImageSize(file.getPath())[1]);
-                    }
-                });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
-            if (data != null) {
-                ArrayList<String> photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
-
-                File imgFile = new File(photos.get(0));
-                fileSize.setText(imgFile.length() / 1024 + "k");
-                imageSize.setText(Luban.get(this).getImageSize(imgFile.getPath())[0] + " * " + Luban.get(this).getImageSize(imgFile.getPath())[1]);
-
-                for (int i = 0; i < photos.size(); i++)
-                    compressWithLs(new File(photos.get(i)));
-            }
-        }
-    }
+  }
 }
